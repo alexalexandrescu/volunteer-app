24f93430f67a65da53c6927d3149cb8befacb3e6
Alex Alexandrescu <alexandrescu.alex@gmail.com>
2020-06-08 13:32:25 +0200

    Experimenting with firebase web hosting

---
+++ web/src/SignIn.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+import * as firebaseConfig from './firebase-config.json'
+
+firebase.initializeApp(firebaseConfig);
+
+class SignInScreen extends React.Component {
+  unregisterAuthObserver!: firebase.Unsubscribe;
+
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <h1>My App</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      );
+    }
+    let defaultAuth = firebase.auth();
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Welcome {defaultAuth && defaultAuth.currentUser ? defaultAuth.currentUser.displayName : null}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+      </div>
+    );
+  }
+}
+
+export default SignInScreen;
